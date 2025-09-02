32df68593b932302ec16ec25d9fdee29c5a0f18a
sam-cohomies <154028485+sam-cohomies@users.noreply.github.com>
2025-09-02 03:59:43 +0000

    WIP 2025-09-02

--- src/impl/governor/VotingPowerRegistry.sol
+++ src/impl/governor/VotingPowerRegistry.sol
@@ -8,6 +8,8 @@ import {Checkpoints} from "@openzeppelin/contracts/utils/structs/Checkpoints.sol
 contract VotingPowerRegistry is IVotingPowerRegistry {
     using Checkpoints for Checkpoints.Trace208;
 
+    error InsufficientVotingPower();
+
     // Mapping to store user voting power allocations
     mapping(address => Checkpoints.Trace208) private userVotingPower;
     // Total voting power at a specific block
@@ -15,12 +17,17 @@ contract VotingPowerRegistry is IVotingPowerRegistry {
     // Unclaimed voting power at a specific block
     Checkpoints.Trace208 private totalUnclaimedVotingPower;
 
+    constructor() {
+        totalVotingPower.push(uint48(block.number), 0);
+        totalUnclaimedVotingPower.push(uint48(block.number), 0);
+    }
+
     function getTotalVotingPower() external view override returns (uint208 votingPower) {
         return totalVotingPower.latest();
     }
 
     function getTotalVotingPowerAtBlock(uint48 _blockNumber) external view override returns (uint208 votingPower) {
-        return totalVotingPower.upperLookup(_blockNumber);
+        return totalVotingPower.upperLookupRecent(_blockNumber);
     }
 
     function getUserVotingPowerAtBlock(address _user, uint48 _blockNumber)
@@ -29,7 +36,7 @@ contract VotingPowerRegistry is IVotingPowerRegistry {
         override
         returns (uint208 votingPower)
     {
-        return userVotingPower[_user].upperLookup(_blockNumber);
+        return userVotingPower[_user].upperLookupRecent(_blockNumber);
     }
 
     function addAllocation(address _user, uint208 _amount) external override {
@@ -39,7 +46,7 @@ contract VotingPowerRegistry is IVotingPowerRegistry {
 
     function removeAllocation(address _user, uint208 _amount) external override {
         if (userVotingPower[_user].latest() < _amount) {
-            revert("Insufficient voting power");
+            revert InsufficientVotingPower();
         }
         userVotingPower[_user].push(uint48(block.number), userVotingPower[_user].latest() - _amount);
         totalVotingPower.push(uint48(block.number), totalVotingPower.latest() - _amount);
--- src/interfaces/governor/IVotingPowerRegistry.sol
+++ src/interfaces/governor/IVotingPowerRegistry.sol
@@ -3,21 +3,24 @@
 pragma solidity ^0.8.30;
 
 interface IVotingPowerRegistry {
-    function getTotalVotingPower() external view returns (uint208);
-    function getTotalVotingPowerAtBlock(uint48 _blockNumber) external view returns (uint208);
+    function getTotalVotingPower() external view returns (uint208 votingPower);
+    function getTotalVotingPowerAtBlock(uint48 _blockNumber) external view returns (uint208 votingPower);
     /**
      * @notice Returns the voting power of a user at a specific block.
      * @param _user The address of the user.
      * @param _blockNumber The block number at which to check the voting power.
-     * @return The voting power of the user at the specified block.
+     * @return votingPower The voting power of the user at the specified block.
      */
-    function getUserVotingPowerAtBlock(address _user, uint48 _blockNumber) external view returns (uint208);
+    function getUserVotingPowerAtBlock(address _user, uint48 _blockNumber)
+        external
+        view
+        returns (uint208 votingPower);
 
     function addAllocation(address _user, uint208 _amount) external;
 
     function removeAllocation(address _user, uint208 _amount) external;
 
-    function getTotalUnclaimedVotingPowerAtBlock(uint48 _blockNumber) external view returns (uint208);
+    function getTotalUnclaimedVotingPowerAtBlock(uint48 _blockNumber) external view returns (uint208 votingPower);
 
-    function getTotalUnclaimedVotingPower() external view returns (uint208);
+    function getTotalUnclaimedVotingPower() external view returns (uint208 votingPower);
 }
